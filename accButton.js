02f5beea8a7da6cc1a4784a4726c89bbb4d68d94
Name <kohany.d@gmail.com>
2022-05-29 01:10:29 +0300

    Moved account button script

---
+++ accButton.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", () => {
+	let space = document.querySelector("a.acc")
+	space.innerHTML = (window.sessionStorage.usr === null)? "Account" : window.sessionStorage.usr
+})
