8708158b5eb7903a296568b5f47e4a7ac53cd2b7
Name <kohany.d@gmail.com>
2022-05-29 19:35:08 +0300

    Modified excercise

--- switch.js
+++ switch.js
@@ -1,30 +1,29 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-let btn = document.querySelector("input#login");
-console.log("IT WORKS")
-
-btn.addEventListener('click', () => {
-	let showing = document.querySelector("div#login");
-	let hiding = document.querySelector("div#register");
-	showing.setAttribute("class", "login-form");
-	hiding.setAttribute("class", "login-form hide");
-	}
-);
-
-btn = document.querySelector("input#register");
-btn.addEventListener('click', () => {
-	let showing = document.querySelector("div#register");
-	let hiding = document.querySelector("div#login");
-	showing.setAttribute("class", "login-form");
-	hiding.setAttribute("class", "login-form hide");
-	}
-);
-
-btn = document.querySelector("input#login-button")
-let logfield = document.querySelector("input#username")
-btn.addEventListener("click", () => {
-	console.log(logfield)
-	console.log(logfield.value)
-	window.sessionStorage.setItem("usr", logfield.value)
-})
+	let btn = document.querySelector("input#login");
+	
+	btn.addEventListener('click', () => {
+		let showing = document.querySelector("div#login");
+		let hiding = document.querySelector("div#register");
+		showing.setAttribute("class", "login-form");
+		hiding.setAttribute("class", "login-form hide");
+		}
+	);
+	
+	btn = document.querySelector("input#register");
+	btn.addEventListener('click', () => {
+		let showing = document.querySelector("div#register");
+		let hiding = document.querySelector("div#login");
+		showing.setAttribute("class", "login-form");
+		hiding.setAttribute("class", "login-form hide");
+		}
+	);
+	
+	btn = document.querySelector("input#login-button")
+	let logfield = document.querySelector("input#username")
+	btn.addEventListener("click", () => {
+		console.log(logfield)
+		console.log(logfield.value)
+		window.sessionStorage.setItem("usr", logfield.value)
+	})
 })
--- views/Register.js
+++ views/Register.js
@@ -27,7 +27,36 @@ export default class extends AbstractView {
     				<input id="login-button" type="button" value="Login" class="login-item">
     			</div>
     		</div>
-		<script type="module" src="switch.js">
+		<script>
+			document.addEventListener("DOMContentLoaded", () => {
+				let btn = document.querySelector("input#login");
+				
+				btn.addEventListener('click', () => {
+					let showing = document.querySelector("div#login");
+					let hiding = document.querySelector("div#register");
+					showing.setAttribute("class", "login-form");
+					hiding.setAttribute("class", "login-form hide");
+					}
+				);
+				
+				btn = document.querySelector("input#register");
+				btn.addEventListener('click', () => {
+					let showing = document.querySelector("div#register");
+					let hiding = document.querySelector("div#login");
+					showing.setAttribute("class", "login-form");
+					hiding.setAttribute("class", "login-form hide");
+					}
+				);
+				
+				btn = document.querySelector("input#login-button")
+				let logfield = document.querySelector("input#username")
+				btn.addEventListener("click", () => {
+					console.log(logfield)
+					console.log(logfield.value)
+					window.sessionStorage.setItem("usr", logfield.value)
+				})
+			})
+		</script>
         `;
     }
 }
