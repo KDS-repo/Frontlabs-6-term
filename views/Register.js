5cd29015a77b1b9544f6cc41db43a37cd9d81f4b
Name <kohany.d@gmail.com>
2022-05-29 17:56:22 +0300

    Modified excercise

--- views/Register.js
+++ views/Register.js
@@ -8,25 +8,26 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-				<div>
-					<header>
-						<input id="register" type="button" value="Register" class="header-button header-text">
-						<input id="login" type="button" value="Login" class="header-button header-text">
-					</header>
-					<div id="register" class="login-form hide">
-						<span class="login-item formtext">Register new user</span>
-						<input type="text" placeholder="E-mail address" class="login-item">
-						<input type="text" placeholder="Username" class="login-item">
-						<input type="text" placeholder="Password" class="login-item">
-						<input type="button" value="Register" class="login-item">
-					</div>
-					<div id="login" class="login-form">
-						<span class="login-item formtext">Login</span>
-						<input id="username" type="text" placeholder="Username" class="login-item">
-						<input id="password" type="text" placeholder="Password" class="login-item">
-						<input id="login-button" type="button" value="Login" class="login-item">
-					</div>
-				</div>
+    		<div class="center">
+    			<header>
+    				<input id="register" type="button" value="Register" class="header-button header-text">
+    				<input id="login" type="button" value="Login" class="header-button header-text">
+    			</header>
+    			<div id="register" class="login-form hide">
+    				<span class="login-item formtext">Register new user</span>
+    				<input type="text" placeholder="E-mail address" class="login-item">
+    				<input type="text" placeholder="Username" class="login-item">
+    				<input type="text" placeholder="Password" class="login-item">
+    				<input type="button" value="Register" class="login-item">
+    			</div>
+    			<div id="login" class="login-form">
+    				<span class="login-item formtext">Login</span>
+    				<input id="username" type="text" placeholder="Username" class="login-item">
+    				<input id="password" type="text" placeholder="Password" class="login-item">
+    				<input id="login-button" type="button" value="Login" class="login-item">
+    			</div>
+    		</div>
+		<script type="module" src="switch.js">
         `;
     }
 }
