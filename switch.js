2161960526a70eb6d2831cada635e20985fb247e
Name <kohany.d@gmail.com>
2022-05-30 14:24:34 +0300

    Modified login script

--- switch.js
+++ switch.js
@@ -1,29 +1,22 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-	let btn = document.querySelector("input#login");
-	
-	btn.addEventListener('click', () => {
+document.addEventListener("click", event => {
+	if(event.target.matches("input#login")) {
 		let showing = document.querySelector("div#login");
 		let hiding = document.querySelector("div#register");
 		showing.setAttribute("class", "login-form");
 		hiding.setAttribute("class", "login-form hide");
-		}
-	);
+	}
 	
-	btn = document.querySelector("input#register");
-	btn.addEventListener('click', () => {
+	if(event.target.matches("input#register")) {
 		let showing = document.querySelector("div#register");
 		let hiding = document.querySelector("div#login");
 		showing.setAttribute("class", "login-form");
 		hiding.setAttribute("class", "login-form hide");
-		}
-	);
+	}
 	
-	btn = document.querySelector("input#login-button")
-	let logfield = document.querySelector("input#username")
-	btn.addEventListener("click", () => {
+	if(event.target.matches("input#login-button")) {
+		let logfield = document.querySelector("input#username")
 		console.log(logfield)
 		console.log(logfield.value)
 		window.sessionStorage.setItem("usr", logfield.value)
-	})
+	}
 })
